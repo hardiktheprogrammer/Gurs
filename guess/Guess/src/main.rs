799e32fcbb786e05846cbb332e7f087a420d3492
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-03-07 17:24:10 +0530

    add guess function

--- guess/Guess/src/main.rs
+++ guess/Guess/src/main.rs
@@ -1,1 +1,8 @@
+io::stdin
 
+fn main() {
+    println!("Guess the number!");
+    println!("Please Input your Guess");
+
+    let mut guess = String::new();
+}
---
+++ guess/Guess/target/debug/deps/Guess.d
@@ -0,0 +1,5 @@
+D:\Gurs\guess\Guess\target\debug\deps\Guess.exe: src\main.rs
+
+D:\Gurs\guess\Guess\target\debug\deps\Guess.d: src\main.rs
+
+src\main.rs:
