83197f6378b71c69308630299d334fd14ede9084
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-03-08 22:44:56 +0530

    add ordering number

--- guess/Guess/src/main.rs
+++ guess/Guess/src/main.rs
@@ -4,16 +4,24 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..-100);
+    let secret_number = rand::thread_rng().gen_range(0_u32, 100_u32);
     println!("Please Input your Guess:{}", secret_number);
-    println!("Please Input Your Guess.");
+    loop {
+        println!("Please Input Your Guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("please Type a  number!"); // guess
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Less => println!("Too Big!"),
+            Ordering::Greater => println!("You win!"),
+            println!("You win");
+            break;
+        }
+    }
 
     // let secret_number: i32
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
 }
